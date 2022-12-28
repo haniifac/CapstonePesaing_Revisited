051b7b6db742677657ed7d2743eac3e3be41b35b
haniifac <haniifcandra@gmail.com>
2022-12-28 18:35:54 +0700

    bottom navigation baru ada tapi belom fix

--- app/src/main/java/com/haniifac/capstonepesaing_revisited/app/ui/MainActivity.kt
+++ app/src/main/java/com/haniifac/capstonepesaing_revisited/app/ui/MainActivity.kt
@@ -2,11 +2,29 @@ package com.haniifac.capstonepesaing_revisited.app.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.haniifac.capstonepesaing_revisited.R
+import com.haniifac.capstonepesaing_revisited.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNav.setupWithNavController(navController)
+
     }
 }
